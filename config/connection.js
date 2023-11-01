32e0e83b558b8f6df027c73a19fc55dcb096db99
Ivan Lopez <84734876+lopezivan763@users.noreply.github.com>
2023-11-01 01:06:52 -0500

    make it better

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: process.env.HOST || 'localhost',
+      host:'localhost',
       dialect: 'mysql',
       port: process.env.PORT || 3306,
     }
