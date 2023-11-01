9384705c1456c19080f0882854bf1333a8549d5c
Ivan Lopez <84734876+lopezivan763@users.noreply.github.com>
2023-11-01 01:15:35 -0500

    make it better

--- config/connection.js
+++ config/connection.js
@@ -3,19 +3,19 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host:'127.0.0.1',
+      host:'localhost',
       dialect: 'mysql',
-      port: process.env.PORT || 3306,
+      port: 3306,
     }
   );
-}
+// }
 
 module.exports = sequelize;
